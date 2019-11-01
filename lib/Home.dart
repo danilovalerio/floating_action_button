33871a7a4eeee85a8f3acc79a585fee31e3b43fc
dvalerio <dnotms290>
2019-11-01 12:45:08 -0300

    Efeitos no botão

--- lib/Home.dart
+++ lib/Home.dart
@@ -9,28 +9,38 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("FloatingActionButton"),),
+      appBar: AppBar(
+        title: Text("FloatingActionButton"),
+      ),
       body: Text("Conteúdo"),
 
       floatingActionButtonLocation:
 //      FloatingActionButtonLocation.endFloat,
-      FloatingActionButtonLocation.endDocked,
+          FloatingActionButtonLocation.endDocked,
 
-      floatingActionButton: FloatingActionButton(
+//      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Colors.green,
         foregroundColor: Colors.yellow,
         elevation: 10,
+        icon: Icon(Icons.add_shopping_cart),
+        label: Text("Adicionar"),
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(50)
+        ),
+
 //        mini: true,
-        child: Icon(Icons.add),
-        onPressed: (){
-          print("Resultado: botão pressionado!");
-        },
+//        child: Icon(Icons.add),
+//        onPressed: (){
+//          print("Resultado: botão pressionado!");
+//        },
       ),
       bottomNavigationBar: BottomAppBar(
+//        shape: CircularNotchedRectangle(),
         child: Row(
           children: <Widget>[
             IconButton(
-              onPressed: (){},
+              onPressed: () {},
               icon: Icon(Icons.menu),
             )
           ],
